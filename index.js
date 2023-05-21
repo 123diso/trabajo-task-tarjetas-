e4e2f6f70f6ae89002bed0af941e92ecf0d7a59c
123diso <127856684+123diso@users.noreply.github.com>
2023-05-21 12:25:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,111 +1,187 @@
-
+// Clase Task
 class Task {
-    constructor(title, status) {
-      this.title = title;
-      this.status = status;
+    constructor(text, state) {
+      this.text = text;
+      this.state = state;
     }
   }
   
+ 
+  let todoList = [];
+  let doingList = [];
+  let doneList = [];
   
-  const createButton = document.getElementById('create-button');
-  const newTaskInput = document.getElementById('new-task');
-  const toDoList = document.getElementById('to-do');
+ 
+  function updateLists() {
+    const todoListElement = document.getElementById('todoList');
+    const doingListElement = document.getElementById('doingList');
+    const doneListElement = document.getElementById('doneList');
   
-  function createTask() {
-  const title = newTaskInput.value;
-  if (title) {
-  const task = new Task(title, 'to-do');
-  saveTask(task);
-  addTaskToDOM(task);
-  newTaskInput.value = '';
+    todoListElement.innerHTML = '';
+    doingListElement.innerHTML = '';
+    doneListElement.innerHTML = '';
+  
+    todoList.forEach((task, index) => {
+      const taskElement = createTaskElement(task, index, 'todoList');
+      todoListElement.appendChild(taskElement);
+    });
+  
+    doingList.forEach((task, index) => {
+      const taskElement = createTaskElement(task, index, 'doingList');
+      doingListElement.appendChild(taskElement);
+    });
+  
+    doneList.forEach((task, index) => {
+      const taskElement = createTaskElement(task, index, 'doneList');
+      doneListElement.appendChild(taskElement);
+    });
   }
+  
+ 
+  function createTaskElement(task, index, listName) {
+    const taskElement = document.createElement('div');
+    taskElement.classList.add('task');
+  
+    const taskText = document.createElement('span');
+    taskText.textContent = task.text;
+    taskElement.appendChild(taskText);
+  
+    const buttons = document.createElement('div');
+    buttons.classList.add('buttons');
+  
+    const redButton = document.createElement('button');
+    redButton.classList.add('red-button');
+    redButton.textContent = 'Descender';
+    redButton.addEventListener('click', () => {
+      moveTask(index, listName, -1);
+    });
+    buttons.appendChild(redButton);
+  
+    const blueButton = document.createElement('button');
+    blueButton.classList.add('blue-button');
+    blueButton.textContent = 'Ascender';
+    blueButton.addEventListener('click', () => {
+      moveTask(index, listName, 1);
+    });
+    buttons.appendChild(blueButton);
+  
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'X';
+    deleteButton.addEventListener('click', () => {
+      deleteTask(index, listName);
+    });
+    buttons.appendChild(deleteButton);
+  
+    taskElement.appendChild(buttons);
+  
+    return taskElement;
   }
   
-  createButton.addEventListener('click', createTask);
-  
-  function addTaskToDOM(task) {
-  const taskElement = document.createElement('div');
-  taskElement.classList.add('task');
-  taskElement.dataset.status = task.status;
-  
-  const titleElement = document.createElement('div');
-  titleElement.classList.add('title');
-  titleElement.innerText = task.title;
-  
-  const actionsElement = document.createElement('div');
-  actionsElement.classList.add('actions');
-  
-  const moveButton = document.createElement('button');
-  moveButton.classList.add('move-button');
-  moveButton.innerText = 'Move';
-  moveButton.addEventListener('click', moveTask);
-  
-  const deleteButton = document.createElement('button');
-  deleteButton.classList.add('delete-button');
-  deleteButton.innerText = 'X';
-  deleteButton.addEventListener('click', deleteTask);
-  
-  actionsElement.appendChild(moveButton);
-  actionsElement.appendChild(deleteButton);
-  
-  taskElement.appendChild(titleElement);
-  taskElement.appendChild(actionsElement);
-  
-  if (task.status === 'to-do') {
-  toDoList.appendChild(taskElement);
-  } else if (task.status === 'doing') {
-  const doingList = document.getElementById('doing');
-  doingList.appendChild(taskElement);
-  } else if (task.status === 'done') {
-  const doneList = document.getElementById('done');
-  doneList.appendChild(taskElement);
-  }
-  }
 
-  function moveTask() {
-    const taskElement = this.parentElement.parentElement;
-    const currentStatus = taskElement.dataset.status;
-    let newStatus;
-    
-    if (currentStatus === 'to-do') {
-    newStatus = 'doing';
-    } else if (currentStatus === 'doing') {
-    newStatus = 'done';
-    } else if (currentStatus === 'done') {
-    newStatus = 'to-do';
+
+
+
+function moveTask(index, listName, direction) {
+    let sourceList, targetList;
+    if (listName === 'todoList') {
+      sourceList = todoList;
+      targetList = direction === -1 ? doingList : [];
+    } else if (listName === 'doingList') {
+      sourceList = doingList;
+      targetList = direction === -1 ? doneList : todoList;
+    } else if (listName === 'doneList') {
+      sourceList = doneList;
+      targetList = direction === -1 ? [] : doingList;
     }
-    
-    taskElement.dataset.status = newStatus;
-    
-    const taskTitle = taskElement.querySelector('.title').innerText;
-    const task = new Task(taskTitle, newStatus);
-    saveTask(task);
-    
-    taskElement.remove();
-    addTaskToDOM(task);
+  
+    const task = sourceList[index];
+    sourceList.splice(index, 1);
+  
+    let targetIndex;
+    if (direction === -1) {
+      targetIndex = Math.max(index - 1, 0);
+      if (targetList === doingList) {
+        targetIndex = Math.min(targetIndex, targetList.length);
+      }
+    } else if (direction === 1) {
+      targetIndex = Math.min(index + 1, targetList.length);
+      if (targetList === todoList) {
+        targetIndex = Math.max(targetIndex, 0);
+      }
+    } else {
+      targetIndex = targetList.length;
     }
-    
-    function deleteTask() {
-    const taskElement = this.parentElement.parentElement;
-    const taskTitle = taskElement.querySelector('.title').innerText;
-    const task = new Task(taskTitle, taskElement.dataset.status);
-    removeTaskFromStorage(task);
-    taskElement.remove();
+  
+    targetList.splice(targetIndex, 0, task);
+  
+    updateLists();
+    saveListsToLocalStorage();
+  }
+  
+  
+
+  function deleteTask(index, listName) {
+    let list;
+    if (listName === 'todoList') {
+      list = todoList;
+    } else if (listName === 'doingList') {
+      list = doingList;
+    } else if (listName === 'doneList') {
+      list = doneList;
     }
-    
-    const removeTaskFromStorage = (task) => {
-        let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-        tasks = tasks.filter(t => !(t.title === task.title && t.status === task.status));
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-      };
+  
+    list.splice(index, 1);
+  
+    updateLists();
+    saveListsToLocalStorage();
+  }
+  
+ 
+  function saveListsToLocalStorage() {
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+    localStorage.setItem('doingList', JSON.stringify(doingList));
+    localStorage.setItem('doneList', JSON.stringify(doneList));
+  }
+  
 
-    const saveTask = (task) => {
-  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-  tasks.push(task);
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-};
+  function loadListsFromLocalStorage() {
+    const storedTodoList = localStorage.getItem('todoList');
+    if (storedTodoList) {
+      todoList = JSON.parse(storedTodoList);
+    }
+  
+    const storedDoingList = localStorage.getItem('doingList');
+    if (storedDoingList) {
+      doingList = JSON.parse(storedDoingList);
+    }
+  
+    const storedDoneList = localStorage.getItem('doneList');
+    if (storedDoneList) {
+      doneList = JSON.parse(storedDoneList);
+    }
+  }
+  
 
-
-
-
+  const addButton = document.getElementById('addButton');
+  addButton.addEventListener('click', () => {
+    const taskInput = document.getElementById('taskInput');
+    const taskText = taskInput.value.trim();
+  
+    if (taskText !== '') {
+      const task = new Task(taskText, 'To Do');
+      todoList.push(task);
+  
+      updateLists();
+      saveListsToLocalStorage();
+  
+      taskInput.value = '';
+    }
+  });
+  
+  
+  window.addEventListener('load', () => {
+    loadListsFromLocalStorage();
+    updateLists();
+  });
+  
 
